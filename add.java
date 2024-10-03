8c0d44a413bd51e6a25a083b3f304835cf2a5db6
SwathiRajmohan <swathiche0@gmail.com>
2024-10-03 14:35:30 +0530

    first

---
+++ add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class add{
+    public static void main(String str[]){
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=a+b;
+        System.out.println(c);
+        scan.close();
+    }
+}
